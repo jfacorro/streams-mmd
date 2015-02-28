a0f18734531a94272b683cceaeb2c9b01b8b5b24
Juan Facorro <juan.facorro@gmail.com>
2015-02-28 18:27:30 -0300

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to streams-mmd
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/streams_mmd/core_test.clj
@@ -0,0 +1,7 @@
+(ns streams-mmd.core-test
+  (:require [clojure.test :refer :all]
+            [streams-mmd.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
