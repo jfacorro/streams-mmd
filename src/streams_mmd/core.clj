9766ac58c4535aa98c636df867c0b1d24eb27de6
Juan Facorro <juan.facorro@gmail.com>
2015-03-01 00:03:37 -0300

    Excercise 4.

--- src/streams_mmd/core.clj
+++ src/streams_mmd/core.clj
@@ -1,5 +1,9 @@
 (ns streams-mmd.core)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 3: Moments - Surprise Number
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn ams-element [c t]
   (if (zero? (mod t c))
     c
@@ -7,9 +11,8 @@
 
 (defn ams-occurrences [c n t a]
   (let [diff  (- n t)
-        m     (int (/ diff c))
-        delta (if (<= a (mod diff c)) 1 0)]
-    (+ m delta)))
+        m     (int (/ diff c))]
+    (inc m)))
 
 (defn ams-random 
   [c n t]
@@ -22,11 +25,42 @@
     (/ (count s))))
 
 (let [f (partial map (partial ams-random 10 75))
-      cases [[22 42 62]
-             [25 34 47]
-             [5 33 67]
-             [4 31 72]]]
+      cases [[3 45 72]
+             [9 50 68]
+             [14 35 42]
+             [25 34 47]]]
+  #_(prn (map f cases))
   (->> cases
     (map f) 
     (map avg) 
-    (map (partial - 562))))
+    (map (partial - 565))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 4: Counting Distinct Elements - Flagolet-Martin
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn hash-fn [x]
+  (mod (+ 7 (* 3 x)) 11))
+
+(defn trailing-zeros [x]
+  (loop [n 0
+         x x]
+    (cond
+      (zero? x) n
+      (zero? (bit-and x 1))
+        (recur (inc n) (bit-shift-right x 1))
+      :else
+        n)))
+
+(let [f (partial map (comp trailing-zeros hash-fn))
+      cases [[4 5 6 10]
+             [2 6 8 10]
+             [2 4 6 10]
+             [2 6 8 9]]]
+  (->>
+    (map (juxt identity f) cases)
+    (filter (fn [[x y]] (= (apply max y) 2)))
+    ffirst))
+
+
+
